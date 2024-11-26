f936f931c46e16c35d036145f7a03a4cc52773da
ahmed ismail <87954455+ahmed777ismail@users.noreply.github.com>
2024-11-26 07:27:17 +0200

    Add files via upload
    Arpa’s hard exam and Mehrdad’s naive cheat

---
+++ Arpa’s hard exam and Mehrdad’s naive cheat/Arpa’s hard exam and Mehrdad’s naive cheat/Arpa’s hard exam and Mehrdad’s naive cheat.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    if (n == 0) {
+        cout << 1 << endl; // 1378^0 = 1
+    }
+    else {
+        int cycle[] = { 6, 8, 4, 2 }; // Last digits cycle
+        cout << cycle[n % 4] << endl;
+    }
+
+    return 0;
+}
